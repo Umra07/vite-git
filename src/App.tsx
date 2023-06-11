bd89a02a21b97f4dc89b06f1eb25d31d6e99f290
Volodymyr Karpenko <vol.karpenko07@gmail.com>
2023-06-11 13:46:16 +0200

    U-001: add Redux Toolkit, React Router DOM, normalize. Configure store and basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,45 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import Router from './pages/router';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <BrowserRouter>
+      <Router />
+      <ul
+        style={{
+          display: 'flex',
+          listStyle: 'none',
+          gap: '20px',
+        }}
+        className="nav">
+        <li>
+          <NavLink
+            style={{
+              padding: '10px',
+              backgroundColor: 'blueviolet',
+              color: '#fff',
+              textDecoration: 'none',
+              borderRadius: '5px',
+            }}
+            to="">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={{
+              padding: '10px',
+              backgroundColor: 'blue',
+              color: '#fff',
+              textDecoration: 'none',
+              borderRadius: '5px',
+            }}
+            to="about">
+            About
+          </NavLink>
+        </li>
+      </ul>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Router.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import { routerType } from '../types/router.types';
+import pagesData from './pagesData';
+
+const Router = () => {
+  const pagesRoutes = pagesData.map(({ path, title, element }: routerType) => (
+    <Route key={title} path={`/${path}`} element={element} />
+  ));
+
+  return <Routes>{pagesRoutes}</Routes>;
+};
+
+export default Router;
